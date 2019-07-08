c40b7c65a14c49cda87723d79993f848a95590c5
KyRillosBoshRa <keroboshra.kb@gmail.com>
2019-07-08 21:25:05 +0200

    new algos

--- Library/Strings/z_function.cpp
+++ Library/Strings/z_function.cpp
@@ -1,10 +1,14 @@
-vector<int> z_function(string s) {
-  int n = s.length();
+vector<int> z(string s) {
+  int n = s.size();
   vector<int> z(n);
-  for (int i = 1, l = 0, r = 0; i < n; ++i) {
-    if (i <= r) z[i] = min(r - i + 1, z[i - l]);
-    while (i + z[i] < n && s[z[i]] == s[i + z[i]]) ++z[i];
-    if (i + z[i] - 1 > r) l = i, r = i + z[i] - 1;
+  int x = 0, y = 0;
+  for (int i = 1; i < n; ++i) {
+    z[i] = max(0, min(z[i - x], y - i + 1));
+    while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+      x = i;
+      y = i + z[i];
+      ++z[i];
+    }
   }
   return z;
 }
